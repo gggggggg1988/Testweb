57ab87f16121c2263b7aab53e3103e767f728106
Lee <807117830@qq.com>
2018-01-28 21:57:20 +0800

    添加jar包

---
+++ FirstApp/src/test/MainTest.java
@@ -0,0 +1,47 @@
+package test;
+
+import org.junit.Test; 
+import org.junit.Before; 
+import org.junit.After;
+import sun.applet.Main;
+
+/** 
+* Main Tester. 
+* 
+* @author <Authors name> 
+* @since <pre>Ò»ÔÂ 27, 2018</pre> 
+* @version 1.0 
+*/ 
+public class MainTest { 
+
+@Before
+public void before() throws Exception { 
+} 
+
+@After
+public void after() throws Exception { 
+} 
+
+/** 
+* 
+* Method: getSession() 
+* 
+*/ 
+@Test
+public void testGetSession() throws Exception { 
+//TODO: Test goes here...
+
+} 
+
+/** 
+* 
+* Method: main(final String[] args) 
+* 
+*/ 
+@Test
+public void testMain() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+
+} 
